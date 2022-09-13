5b30404dd9fe6278cee0f69d0a1befb81e25a5fc
Bartodziejj <bartixbo147@gmail.com>
2022-09-13 11:39:23 +0200

    modyfikacja settings

--- osoby/views.py
+++ osoby/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Ohayo!")
+    return HttpResponse("<h1>Jesse, we need to cook.</h1>")
 # Create your views here.
